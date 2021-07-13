0a63733754715f397115d93acbee1e4071d27de7
Alex Albaran <albaranlex@icloud.com>
2021-07-13 11:12:43 +0200

    created counter.json and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavbar from "./components/TopNavbar";
 import Main from "./components/Main";
 import Posts from "./components/Posts";
 import Images from "./components/Images";
+import Counters from "./counters.json";
 
 function App() {
+  const [counter, setCounter] = useState(Counters);
+  console.log(Counters);
+
   return (
     <div className="App">
       <TopNavbar />
       <Main />
       <Posts />
-      <Images />
+      <Images counter={counter} />
     </div>
   );
 }
